942b066aa8b2af94f15a78c7944a7b1a84251b36
djangouser <ngn2349603@stu.o-hara.ac.jp>
2024-10-29 13:54:13 +0900

    商品追加

--- dgroupapp/admin.py
+++ dgroupapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# modelsからCategory、FoodInput、Allergyクラスをインポート
+from .models import Category, FoodInput, Allergy
 
-# Register your models here.
+
+# 管理サイトにCategory、FoodInput、Allergyを登録する
+admin.site.register(Category)
+admin.site.register(FoodInput)
+admin.site.register(Allergy)
--- dgroupapp/views.py
+++ dgroupapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DeleteView
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import FoodInputForm
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-# Create your views here.
+
+
+# 商品情報入力ページのビュー
+class CreateFoodView(CreateView):
+    form_class = FoodInputForm
+    template_name = "input_food.html"
+    success_url = reverse_lazy('dgroupapp:input_done')
+
+    def form_valid(self, form):
+        inputdata = form.save(commit=False)
+        inputdata.save()
+        return super().form_valid(form)
+
+
+# 入力完了ページのビュー
+class InputDoneView(TemplateView):
+    template_name = 'input_done.html'
