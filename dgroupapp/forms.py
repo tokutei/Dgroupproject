a82b129e13d99804359da83675b5adea6dfd4897
djangouser <ngn2349603@stu.o-hara.ac.jp>
2024-10-29 13:46:25 +0900

    商品追加機能(一部)

---
+++ dgroupapp/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm
+from .models import FoodInput
+from django import forms
+
+
+class FoodInputForm(ModelForm):
+
+    # allergy = forms.MultipleChoiceField(
+    #     label='アレルギー',
+    #     required=False,
+    #     disabled=False,
+    #     initial=[],
+    #     choices=[(i.name, i.name) for i in Allergy.objects.all()],
+    #     widget=forms.CheckboxSelectMultiple
+    # )
+
+    class Meta:
+        model = FoodInput
+        fields = ['category', 'name', 'price', 'stock', 'shelf_life', 'image']
+        widgets = {
+            'shelf_life': forms.SelectDateWidget()
+        }
