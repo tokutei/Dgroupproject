bd4d730ab08350a1fba5ec85aa699b4ada3b05c6
djangouser <ngn2349603@stu.o-hara.ac.jp>
2024-11-06 12:12:04 +0900

    コメントアウト戻しました

--- dgroupapp/forms.py
+++ dgroupapp/forms.py
@@ -1,20 +1,18 @@
 from django.forms import ModelForm
-from .models import FoodInput
-
-
+from .models import FoodInput, Allergy
 from django import forms
 
 
 class FoodInputForm(ModelForm):
 
-    # allergy = forms.MultipleChoiceField(
-    #     label='アレルギー',
-    #     required=False,
-    #     disabled=False,
-    #     initial=[],
-    #     choices=[(i.name, i.name) for i in Allergy.objects.all()],
-    #     widget=forms.CheckboxSelectMultiple
-    # )
+    allergy = forms.MultipleChoiceField(
+        label='アレルギー',
+        required=False,
+        disabled=False,
+        initial=[],
+        choices=[(i.name, i.name) for i in Allergy.objects.all()],
+        widget=forms.CheckboxSelectMultiple
+    )
 
     class Meta:
         model = FoodInput
