2a25024bde58bb6905c8fe660e9a42232813309e
月火 <ngn2349535@stu.o-hara.ac.jp>
2024-11-11 14:47:21 +0900

    お問い合わせ機能

--- adminmenu/models.py
+++ adminmenu/models.py
@@ -1,13 +1,3 @@
 from django.db import models
-from django.db import models
 
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
-    address = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)  # 送信日時を自動で記録
-
-    def __str__(self):
-        return self.name
+# Create your models here.
--- adminmenu/views.py
+++ adminmenu/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Contact
+from dgrouinquiry.models import Contact
 
 
 def system_menu(request):
