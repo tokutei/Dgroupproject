466db678ba1ee9a216ab9c016bf800e9bfa12912
djangouser <ngn2349603@stu.o-hara.ac.jp>
2024-11-20 11:49:23 +0900

    食品編集機能追加

--- foods/urls.py
+++ foods/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('input_done/', views.InputSuccessView.as_view(), name='input_done'),
     path('food_delete/', views.FoodDeleteListView.as_view(), name='food_delete_list'), 
     path('food_delete/<int:pk>/', views.FoodDeleteView.as_view(), name='food_delete'),
+    path('food_update/<int:pk>', views.UpdateFoodView.as_view(), name='food_update'),
 ]
--- foods/views.py
+++ foods/views.py
@@ -1,14 +1,13 @@
 import stripe
 from django.conf import settings
 from django.shortcuts import render
-from django.views.generic import TemplateView, DeleteView, CreateView
+from django.views.generic import TemplateView, DeleteView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.http import JsonResponse
 from purchaseapp.models import Product, Price
 from .forms import FoodInputForm
 from django.views.generic import ListView, DeleteView 
 from .models import Food  
-
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -58,6 +57,7 @@ class CreateFoodView(CreateView):
 class InputSuccessView(TemplateView):
     template_name = 'input_success.html'
 
+
 # 登録食品の一覧ページ
 class FoodDeleteListView(ListView):
     model = Food
@@ -70,3 +70,44 @@ class FoodDeleteView(DeleteView):
     model = Food
     template_name = 'food_delete_confirm.html'
     success_url = reverse_lazy('foods:food_delete_list')
+
+
+# 編集ページのビュー
+class UpdateFoodView(UpdateView):
+    template_name = 'update_food.html'
+    form_class = FoodInputForm
+    model = Food
+    success_url = reverse_lazy('foods:food_delete_list')
+
+    def form_valid(self, form):
+        adddata = form.save(commit=False)
+        allergy = form.cleaned_data.get("allergy")
+        if allergy:
+            adddata.allergy = '、'.join(allergy)
+        else:
+            adddata.allergy = 'なし'
+        productname = adddata.name
+        price = int(adddata.price)
+        product = stripe.Product.create(
+            name=productname,
+        )
+        price_data = stripe.Price.create(
+            product=product.id,
+            unit_amount=price,
+            currency='jpy'
+        )
+        productdb = Product(
+            name=productname,
+            stripe_product_id=product.id,
+        )
+        productdb.save()
+        price = Price(
+            product=productdb,
+            stripe_price_id=price_data.id,
+            price=price,
+        )
+        adddata.stripe_product_id = product.id
+        adddata.stripe_price_id = price_data.id
+        price.save()
+        adddata.save()
+        return super().form_valid(form)
