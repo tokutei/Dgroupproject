a9502998b5d48df38829038d10bb19a6637151e2
山崎 <ngn2349549@stu.o-hara.ac.jp>
2024-11-21 12:45:40 +0900

    検索機能修正

--- dgroupapp/views.py
+++ dgroupapp/views.py
@@ -17,19 +17,22 @@ class IndexView(ListView):
     context_object_name = 'object_list'
 
     def get_queryset(self):
-        queryset = Food.objects.order_by('-inputed_at')[:8]
+        # 先に検索条件でフィルタリング
+        queryset = Food.objects.all()
+
         self.search_query = self.request.GET.get('name', None)
         if self.search_query:
             queryset = queryset.filter(name__icontains=self.search_query)
-        return queryset
+
+        # その後にスライス
+        return queryset.order_by('-inputed_at')[:8]  # フィルタリング後にスライスを適用
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search_query'] = self.search_query
         context['result_count'] = context['object_list'].count()
-        context['categorys'] = Category.objects.all()  # カテゴリ情報を追加
-         # 賞味期限が遠い順に商品4件を取得
-        context['recommended_items'] = Food.objects.order_by('shelf_life')[:4]  # 賞味期限が遠い順に4つ取
+        context['categorys'] = Category.objects.all()
+        context['recommended_items'] = Food.objects.order_by('shelf_life')[:4]
         return context
 
 
