e6bafe5210a4c27fdfd13d3d68afa0733b84c200
月火 <ngn2349535@stu.o-hara.ac.jp>
2024-11-21 12:07:06 +0900

    a

--- dgroupLogin/urls.py
+++ dgroupLogin/urls.py
@@ -5,7 +5,7 @@ from .views import TermsAndPrivacyView
 app_name = 'dgroupLogin'
 
 urlpatterns = [
-    path('terms-and-privacy/', TermsAndPrivacyView.as_view(), name='terms_and_privacy'),  # アンダースコアに修正
+    path('terms-and-privacy/', TermsAndPrivacyView.as_view(), name='terms_and_privacy'),  
     path('phone_number/', views.phone_number_view, name='phone_number'),
     path('user_profile/', views.user_profile_view, name='user_profile'),
     path('confirmation/', views.confirmation_view, name='confirmation'),
